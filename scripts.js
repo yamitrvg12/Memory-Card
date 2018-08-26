edbb856f2e2687c56b5bfb1597e436947b659241
Yamit Villamil <yamit.villamil@globant.com>
2018-08-25 23:07:15 -0500

    Refactoring

--- scripts.js
+++ scripts.js
@@ -9,25 +9,36 @@ function flipCard() {
         // First click
         hasFlippedCard = true;
         firstCard = this; // memory card
-    } else {
-        // second click
-        hasFlippedCard = false;
-        secondCard = this;
-        
-        // do card match
-        if (firstCard.dataset.framework === secondCard.dataset.framework) {
-            // it's a match
-            firstCard.removeEventListener('click', flipCard);
-            secondCard.removeEventListener('click', flipCard);
-        } else {
-            // it's not a match
-            setTimeout(() => {
-                firstCard.classList.remove('flip');
-                secondCard.classList.remove('flip');
-            }, 1500);
-        }
-        console.log('function was executed');
+
+        return;
     }
+
+    // second click
+    hasFlippedCard = false;
+    secondCard = this;
+    
+    checkForMatch();
+}
+
+function checkForMatch() {
+    // do card match
+    const isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+
+    isMatch ? disableCards() : unflipCards();
+}
+
+function disableCards() {
+    // it's a match
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+}
+
+function unflipCards() {
+    // it's not a match
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+    }, 1500);
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
